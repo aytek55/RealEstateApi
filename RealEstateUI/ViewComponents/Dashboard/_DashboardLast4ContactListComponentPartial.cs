df2b7fbae94bc33dc359db2dbf41adec4f4b498b
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2025-02-24 19:47:07 +0300

    - Product ve dashboard düzenlemeleri.

---
+++ RealEstateUI/ViewComponents/Dashboard/_DashboardLast4ContactListComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstateUI.Dtos.ContactDtos;
+
+namespace RealEstateUI.ViewComponents.Dashboard
+{
+    public class _DashboardLast4ContactListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DashboardLast4ContactListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7197/api/Contacts/GetLast4Contact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<Last4ContactResultDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
