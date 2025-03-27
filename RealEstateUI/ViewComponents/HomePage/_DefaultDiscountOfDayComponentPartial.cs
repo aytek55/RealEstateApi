aa2128791d5aba3c7c332edf73b27000d65bf206
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2025-03-27 11:57:49 +0300

    - Hatalar giderildi.

--- RealEstateUI/ViewComponents/HomePage/_DefaultDiscountOfDayComponentPartial.cs
+++ RealEstateUI/ViewComponents/HomePage/_DefaultDiscountOfDayComponentPartial.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using RealEstateUI.Dtos.ProductDtos;
 
 namespace RealEstateUI.ViewComponents.HomePage
 {
@@ -10,21 +11,17 @@ namespace RealEstateUI.ViewComponents.HomePage
         {
             _httpClientFactory = httpClientFactory;
         }
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7197/api/Products/GetLast3Product");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast3ProductWithCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
-        //public async Task<IViewComponentResult> InvokeAsync()
-        //{
-        //    var client = _httpClientFactory.CreateClient();
-        //    var responseMessage = await client.GetAsync("https://localhost:7197/api/Products/GetLast3Product");
-        //    if (responseMessage.IsSuccessStatusCode)
-        //    {
-        //        var jsonData = await responseMessage.Content.ReadAsStringAsync();
-        //        var values = JsonConvert.DeserializeObject<List<ResultLast3ProductWithCategoryDto>>(jsonData);
-        //        return View(values);
-        //    }
-        //    return View();
-        //}
     }
 }
